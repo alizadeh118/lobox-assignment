2797082209d1cf028bd769d151a9f640ffe09d73
Hassan Alizadeh <hasan.alizadeh@arvancloud.ir>
2024-01-18 16:21:47 +0330

    chore: add eslint and prettier configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true
   },
-}
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ["@typescript-eslint", "react", "simple-import-sort", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "react/prop-types": "off",
+    "simple-import-sort/imports": "error",
+  }
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+};
